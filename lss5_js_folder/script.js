2906c20fa55cf5b2cb1eb63bdbeaf463355ca450
NguyenLong <nguyenlong160104@gmail.com>
2024-03-24 21:31:54 +0700

    lesson5 Javascript

---
+++ lss5_js_folder/script.js
@@ -0,0 +1,45 @@
+const pi = 3.14;
+
+// String
+var msg1 = 'String';
+var msg2 = "String";
+var msg3 = 'String';
+console.log(msg1);
+
+// Boolean
+var isDarkMode = true;
+var isDarkMode = false;
+console.log(isDarkMode);
+
+// Undefined 
+var block;
+console.log(block);
+
+// Null
+var c = null;
+console.log(c);
+
+// Function
+function SumNumber(So1,So2) {
+    return So1 + So2;
+}
+var a =SumNumber(10,20);
+console.log(a);
+
+// Object
+var infoUser = {
+    fullName: 'NguyenDucLong',
+    age: 19,
+    gender: 'Male',
+}
+console.log(infoUser);
+console.log(infoUser.fullName);
+
+var listUser = [
+    "Nguyen Duc Long",
+    "Quack Kim Chi",
+    "Le Van A",
+    "Nguyen Van C",
+];
+console.log(listUser);
+console.log(listUser[3]);
---
+++ lss6_js_folder/script.js
@@ -0,0 +1,18 @@
+// Một số hàm built-in ( Được định nghĩa sẵn)
+// alert(message);in ra 1 thông báo popup
+// alert('Hello');
+
+// // confirm
+// var result = confirm("Ban da tren 18 tuoi?");
+// console.log(result);
+
+// prompt
+// var GiatriBandau = prompt("GiatriBandau",0);
+// console.log(GiatriBandau);
+// console.warn(GiatriBandau);
+// console.error(GiatriBandau);
+
+// setTimeout: Chạy 1 lần duy nhất sau 1 khoảng thời gian
+setTimeout(function (){
+    console.log("5 minites");
+}, 5000);
